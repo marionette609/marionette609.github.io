bedb7a77b59dbd2602009d6f596a501e38ea1769
marionette <marionette609@proton.me>
2025-04-10 18:57:40 -0500

    js updates, add DC07 test data

--- script.js
+++ script.js
@@ -1,109 +1,149 @@
-d3.json("https://raw.githubusercontent.com/marionette609/rew-scripts/refs/heads/main/sample-data/dsha-ygg-dBu.json", function(data) {
+let this_url = window.location.href;
+this_url = new URL(this_url);
+let origin = this_url.origin;
 
-    var pfreq = [];
-    var plevel = []
-    var pthd = [];
+if (origin == "null") {
+    origin = "https://marionette609.github.io"
+}
 
-    var distortion_to_plot = "thdPlusN"; // Valid values are thdPlusN, H# e.x. H2
+let dataset = this_url.searchParams.get("dataset");
+let zmax = this_url.searchParams.get("zMax");
+let zmin = this_url.searchParams.get("zMin");
 
-    graph_levels = []
+if (dataset == null) {
+    dataset = "DC07-balanced-300";
+}
 
+if (zmax == null) {
+    zmax = -40;
+}
 
-    data_keys = Object.keys(data);
-    data_keys.sort((x,y) => {
-        floatx = parseFloat(x);
-        floaty = parseFloat(y);
-        return x-y;
-    });
+if (zmin == null) {
+    zmin = -130;
+}
 
-    // iterating data[data_keys]
-    for(var i = 0; i < data_keys.length; i++) {
-        float_freq = parseFloat(data_keys[i]);
-        if(float_freq < 20 || float_freq > 10000) {
-            continue;
-        }
-        pfreq.push(float_freq);
-        pfreq.sort((x,y) => x - y);
-        var this_level = [];
+d3.json(origin+"/data/"+dataset+".json", function(data) {
 
-        level_keys = Object.keys(data[data_keys[i]]);
-        level_keys.sort((x,y) => {
+    function parse_json(distortion_to_plot = "thdPlusN") {
+        let pfreq = [];
+        let plevel = []
+        let pdist = [];
+    
+        data_keys = Object.keys(data);
+        data_keys.sort((x,y) => {
             floatx = parseFloat(x);
             floaty = parseFloat(y);
             return x-y;
         });
-        for(var j = 0; j < level_keys.length; j++) {
-            // iterating data[data_keys[i]][level_keys[j]]
-            var thd = data[data_keys[i]][level_keys[j]];
-            float_level = parseFloat(level_keys[j]);
-            if(!plevel.includes(float_level)){
-                plevel.push(float_level);
-                plevel.sort((x,y) => x - y);
+    
+        // iterating data[data_keys]
+        for(let i = 0; i < data_keys.length; i++) {
+            float_freq = parseFloat(data_keys[i]);
+            if(float_freq < 20 || float_freq > 10000) {
+                continue;
             }
-            if(thd.hasOwnProperty(distortion_to_plot)){
-                this_level.push(parseFloat(thd[distortion_to_plot]));
-            }else{
-                this_level.push(-100.0);
+            pfreq.push(float_freq);
+            pfreq.sort((x,y) => x - y);
+            let this_level = [];
+    
+            level_keys = Object.keys(data[data_keys[i]]);
+            level_keys.sort((x,y) => {
+                floatx = parseFloat(x);
+                floaty = parseFloat(y);
+                return x-y;
+            });
+            for(let j = 0; j < level_keys.length; j++) {
+                // iterating data[data_keys[i]][level_keys[j]]
+                let thd = data[data_keys[i]][level_keys[j]];
+                float_level = parseFloat(level_keys[j]);
+                if(!plevel.includes(float_level)){
+                    plevel.push(float_level);
+                    plevel.sort((x,y) => x - y);
+                }
+                if(thd.hasOwnProperty(distortion_to_plot)){
+                    this_level.push(parseFloat(thd[distortion_to_plot]));
+                }else{
+                    // this_level.push(-140.0);
+                    this_level.push(null);
+                }
             }
+            pdist.push(this_level);
         }
-        graph_levels.push(this_level);
+
+        return [pfreq, plevel, pdist];
     }
 
-
-
-    var data = [{
-        x: plevel,
-        y: pfreq,
-        z: graph_levels,
-        type: 'surface',
-        colorscale: "Rainbow",
-        contours: {
-            z: {
-                show:true,
-                usecolormap: true,
-                highlightcolor:"#42f462",
-                project:{z: true}
+    function format_plot_data(frequency, level, distortion, title) {
+        let plotdata = [{
+            x: level,
+            y: frequency,
+            z: distortion,
+            type: 'surface',
+            colorscale: "Rainbow",
+            contours: {
+                z: {
+                    show:true,
+                    usecolormap: true,
+                    highlightcolor:"#42f462",
+                    project:{z: true}
+                }
             }
-        }
-    }];
-
-    var layout = {
-        title: {
-            text: 'Distortion Surface'
-        },
-        scene: {
-            yaxis: {
-                type: 'log',
-                dtick: 1,
-                autorange: true,
-                title: {
-                    text: "Frequency (y)"
+        }];
+    
+        let layout = {
+            title: {
+                text: "Distortion Surface " + title + " " + dataset
+            },
+            scene: {
+                yaxis: {
+                    type: "log",
+                    dtick: 1,
+                    autorange: true,
+                    title: {
+                        text: "Frequency (y)"
+                    }
+                },
+                xaxis: {
+                    title: {
+                        text: "dBu (x)"
+                    }
+                },
+                zaxis: {
+                    range: [zmin, zmax],
+                    title: {
+                        text: "THD+N (z)"
+                    }
+                },
+                aspectratio: {
+                    "x": 1,
+                    "y": 1,
+                    "z": 1
                 }
             },
-            xaxis: {
-                title: {
-                    text: "dBu (x)"
-                }
-            },
-            zaxis: {
-                range: [-130, -40],
-                title: {
-                    text: "THD+N (z)"
-                }
-            },
-            aspectratio: {
-                "x": 1,
-                "y": 1,
-                "z": 1
-            }
-        },
+    
+            autosize: false,
+            width: 1000,
+            height: 1000
+    
+        };
+        return [plotdata, layout];
+    }
 
-        autosize: false,
-        width: 1000,
-        height: 1000
+    let pfreq, plevel, pdist, dist_to_plot;
 
-    };
+    dist_to_plot = "thdPlusN";
+    [pfreq, plevel, pdist] = parse_json(dist_to_plot);
+    [plotdata, layout] = format_plot_data(pfreq, plevel, pdist, dist_to_plot);
+    Plotly.newPlot('thdplusn', plotdata, layout);
 
-    Plotly.newPlot('myDiv', data, layout);
+    dist_to_plot = "H2";
+    [pfreq, plevel, pdist] = parse_json(dist_to_plot);
+    [plotdata, layout] = format_plot_data(pfreq, plevel, pdist, dist_to_plot);
+    Plotly.newPlot('H2', plotdata, layout);
+
+    dist_to_plot = "H3";
+    [pfreq, plevel, pdist] = parse_json(dist_to_plot);
+    [plotdata, layout] = format_plot_data(pfreq, plevel, pdist, dist_to_plot);
+    Plotly.newPlot('H3', plotdata, layout);
 
 });
